f6702f594409b842ed58829582d4797e537c2988
Pranjay Vats <pranjayvats22062001@gmail.com>
2022-03-09 23:45:12 +0530

    Ecommerce

--- backend/server.js
+++ backend/server.js
@@ -16,14 +16,15 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 
 //Connect to database
 connectDatabase();
+
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// We will use nodemon in the devlopment mode and we will use in the production mode
-app.listen(process.env.PORT, () => {
+// We will use nodemon in the development mode and we will use in the production mode
+const server = app.listen(process.env.PORT, () => {
   console.log(`Server is working on http://localhost:${process.env.PORT}`);
 });
 
@@ -31,6 +32,7 @@ app.listen(process.env.PORT, () => {
 process.on("unhandledRejection", (err) => {
   console.log(`Error: ${err.message}`);
   console.log("Shutting down server due to Unhandled Promise Rejection");
+  
   server.close(() => {
     process.exit(1);
   });
